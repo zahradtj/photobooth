680f4eb2768c9bec58a9b1b11d4e41b6b54fc467
Trey Zahradka <trey.j.zahradka@gmail.com>
2017-06-09 21:59:55 -0500

    take screenshots

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import kivy
-from kivy._clock import ClockEvent
+import time
 from kivy._event import partial
 
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
-kivy.require('1.10.0')  # replace with your current kivy version !
+kivy.require('1.10.0')
+
+
+sm = ScreenManager(transition=NoTransition())
+number_pictures = 3
 
 
 class HomeScreen(Screen):
@@ -19,6 +24,8 @@ class PreviewScreen(Screen):
 
 class PictureScreen(Screen):
     event = None
+    photos = []
+    start_time = None
 
     def countdown(self, dt):
         current = int(self.ids['number'].text)
@@ -26,18 +33,26 @@ class PictureScreen(Screen):
         if current == 0:
             self.ids['number'].text = ""
             self.event.cancel()
+            self.capture_photo()
             return
         self.ids['number'].text = str(current)
 
     def schedule_countdown(self):
+        self.start_time = time.strftime("%Y%m%d%H%M%S")
         self.event = Clock.schedule_interval(partial(self.countdown), 1)
 
+    def capture_photo(self):
+        # Capture picture here
+        photoName = self.start_time + "_{}.jpg".format(len(self.photos))
+        Window.screenshot(name=photoName)
 
-class CountdownScreen(Screen):
-    pass
-
-
-sm = ScreenManager(transition=NoTransition())
+        self.photos.append("picture")
+        print "capture {}".format(len(self.photos))
+        if len(self.photos) < number_pictures:
+            self.ids['number'].text = "5"
+            self.event = Clock.schedule_interval(partial(self.countdown), 1)
+        else:
+            print "print"
 
 
 class PhotoBoothApp(App):
