59e55437787c4058e9156c871ff7d8bb32aaabc4
Trey Zahradka <trey.j.zahradka@gmail.com>
2017-06-11 17:01:21 -0500

    enable multiple photobooth sessions (#8)

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import re
 from PIL import Image
 from kivy.app import App
 from kivy.clock import Clock
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
 kivy.require('1.10.0')
@@ -49,14 +52,14 @@ class PictureScreen(Screen):
         camera.export_to_png(photo_name)
 
         photos.append(photo_name)
-        print(photos)
+        print photos
         print("capture {}".format(len(photos)))
         if len(photos) < number_pictures:
             self.ids['number'].text = "5"
             self.countdown_value = 5
             self.event = Clock.schedule_interval(partial(self.countdown), 1)
         else:
-            camera.play = False
+            self.countdown_value = 5
             sm.current = 'print'
             print("print")
 
@@ -82,27 +85,75 @@ class PrintScreen(Screen):
         image.source = collage_name
 
 
-class EmailScreen(Screen):
-    def insert(self, value):
-        email = re.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+class ConfirmPopup(GridLayout):
+    text = StringProperty()
 
+    def __init__(self, **kwargs):
+        self.register_event_type('on_answer')
+        super(ConfirmPopup, self).__init__(**kwargs)
+
+    def on_answer(self, *args):
+        pass
+
+
+class EmailScreen(Screen):
+    email = re.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
+    def insert(self, value):
         found = False
         for i in self.ids['rv'].data:
             if i['value'] == value:
                 found = True
                 break
 
-        if email.match(value) and not found:
+        if self.email.match(value) and not found:
             self.ids['rv'].data.insert(0, {'value': value})
 
     def test_email(self, value):
-        email = re.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
-        if email.match(value):
+        if self.email.match(value):
             self.ids['email_input'].background_color = [0.9,1,0.9,1]
             self.ids['add_email'].disabled = False
         else:
             self.ids['email_input'].background_color = [1,0.9,0.9,1]
             self.ids['add_email'].disabled = True
+
+    def send(self):
+        start_time = photos[0].split("_")[1]
+        email_file_name = "EMAILS_{}.txt".format(start_time)
+
+        emails = ''
+        index = 1
+        for email in self.ids['rv'].data:
+            emails += email['value']
+            if not index == len(self.ids['rv'].data):
+                emails += ";"
+            index += 1
+
+        with open(email_file_name, "w") as output:
+            output.write(str(emails))
+
+        del photos[:]
+        sm.current = 'home'
+
+    def skip(self):
+        if len(self.ids['rv'].data) != 0:
+            content = ConfirmPopup(text='You have email addresses listed.\n\nSkip sending email?')
+            content.bind(on_answer=self._on_answer)
+            popup = Popup(title="Skip Email?",
+                               content=content,
+                               size_hint=(None, None),
+                               size=(480, 250),
+                               auto_dismiss=False)
+            popup.open()
+        else:
+            del photos[:]
+            sm.current = 'home'
+
+    def _on_answer(self, instance, answer):
+        if answer == 'yes':
+            del photos[:]
+            sm.current = 'home'
+        self.popup.dismiss()
 
 
 class EmailRow(BoxLayout):
